d94f6e486653844e3fd0ea40f42d1ddc63f2b29f
ukrgerri4 <ukrgerri4@gmail.com>
2021-08-23 16:08:01 +0300

    added server-structure uml

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,8 +3,8 @@ import { Router } from  "./router.js";
 const router = new Router();
 router
   .addRoute('/', 'home-template')
-  .addRoute('/ikcv', 'is-being-developed-template') // ikcv-template
-  .addRoute('/manchkin-counter', 'is-being-developed-template') // manchkin-counter-template
+  .addRoute('/ikcv', 'ikcv-template')
+  .addRoute('/manchkin-counter', 'manchkin-counter-template')
   .addRoute('/mediatR-extension', 'mediatR-extension-template')
   .addRoute('/about-me', 'is-being-developed-template') // about-me-template
   .addListeners();
