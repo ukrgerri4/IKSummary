4a66a0208d4b805f8edc56732eebc26908277832
ukrgerri4 <ukrgerri4@gmail.com>
2021-09-06 10:36:13 +0300

    added server structure page

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,9 +3,10 @@ import { Router } from  "./router.js";
 const router = new Router();
 router
   .addRoute('/', 'home-template')
-  .addRoute('/ikcv', 'ikcv-template')
+  .addRoute('/ikcv', 'is-being-developed-template') // ikcv-template
   .addRoute('/manchkin-counter', 'manchkin-counter-template')
   .addRoute('/mediatR-extension', 'mediatR-extension-template')
+	.addRoute('/server-structure', 'server-structure-template')
   .addRoute('/about-me', 'is-being-developed-template') // about-me-template
   .addListeners();
 
