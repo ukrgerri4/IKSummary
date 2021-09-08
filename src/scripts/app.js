a6aee4c87e63336ee0a47ba0142bef66892aeba9
ukrgerri4 <ukrgerri4@gmail.com>
2021-09-08 16:16:04 +0300

    fixed import

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -5,7 +5,7 @@ import manchkin_counter_template from "../templates/manchkin-counter.html";
 import mediatr_extension_template from "../templates/mediatr-extension.html";
 import server_structure_template from "../templates/server-structure.html";
 import is_being_developed_template from "../templates/is-being-developed.html";
-import about_template from "../templates/ikcv.html";
+import about_template from "../templates/about.html";
 
 
 const router = new Router();
@@ -14,6 +14,6 @@ router
   .addRoute('/ikcv', 'ikcv', ikcv_template)
   .addRoute('/manchkin-counter', 'manchkin-counter', manchkin_counter_template)
   .addRoute('/mediatr-extension', 'mediatr-extension', mediatr_extension_template)
-	.addRoute('/server-structure', 'server-structure', server_structure_template)
+  .addRoute('/server-structure', 'server-structure', server_structure_template)
   .addRoute('/about', 'about', is_being_developed_template)
   .addListeners();
